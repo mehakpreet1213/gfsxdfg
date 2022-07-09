8c4295cd666ea4e9a6968ffffb4564f77c12c08a
mehakpreet1213 <72645356+mehakpreet1213@users.noreply.github.com>
2022-07-09 19:57:06 +0530

    Add files via upload

---
+++ js/infoBanner.js
@@ -0,0 +1,111 @@
+AFRAME.registerComponent("info-banner", {
+  schema: {
+    itemId: { default: "", type: "string" },
+  },
+  update: function () {
+    this.createBanner();
+  },
+  createBanner: function () {
+    postersInfo = {
+      superman: {
+        banner_url: "./assets/posters/superman-banner.jpg",
+        title: "Superman",
+        released_year: "1983",
+        description:
+          "Superman is often depicted with a mix of idealism, restraint, fairness, and compassion. The Birthright limited series attributes Superman's compassion for living things to his ability to see their .",
+      },
+      spiderman: {
+        banner_url: "./assets/posters/spiderman-banner.png",
+        title: "Spiderman",
+        released_year: "1962",
+        description:
+          "Spider-Man is a fictional superhero created by writer-editor Stan Lee and writer-artist Steve Ditko. He first appeared in the anthology comic book Amazing Fantasy (Aug. 1962) in the Silver Age of Comic Books.",
+      },
+      "captain-aero": {
+        banner_url: "./assets/posters/captain-aero-banner.jpg",
+        title: "Captain Aero",
+        released_year: "1942",
+        description:
+          "Captain Aero is a Flying ace for the US Army, who patrols the skies with his little Chinese pal, Chop Suey. In his first adventure, he tests out an experimental P-60 plane and escorts a flight of Canadian flyers taking planes to England, as America and not yet entered World War II.",
+      },
+      "outer-space": {
+        banner_url: "./assets/posters/outer-space-banner.jpg",
+        title: "Outer Space",
+        released_year: "1952",
+        description:
+          "Tintin and crew find themselves aboard a rocket ship and eventually on the surface of the moon in this classic adventure. Trouble ensues when a nefarious stowaway is discovered on the ship, jeopardizing the crew and mission. Since the publication of the story predated manned space",
+      },
+    };
+    const { itemId } = this.data;
+
+    const fadeBackgroundEl = document.querySelector("#fadeBackground");
+
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("id", `${itemId}-banner`);
+
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.9,
+      height: 1,
+    });
+
+    entityEl.setAttribute("material", { color: "#000" });
+    entityEl.setAttribute("position", { x: 0, y: 0.1, z: -1 });
+
+    const item = postersInfo[itemId];
+
+    const imageEl = this.createImageEl(item);
+    const titleEl = this.createTitleEl(item);
+    const descriptionEl = this.createDescriptionEl(item);
+
+    entityEl.appendChild(imageEl);
+    entityEl.appendChild(titleEl);
+    entityEl.appendChild(descriptionEl);
+
+    fadeBackgroundEl.appendChild(entityEl);
+  },
+  createImageEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("geometry", {
+      primitive: "plane",
+      width: 0.85,
+      height: 0.4,
+    });
+    entityEl.setAttribute("material", { src: item.banner_url });
+    entityEl.setAttribute("position", { x: 0, y: 0.3, z: 0.05 });
+    return entityEl;
+  },
+  createTitleEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 1.2,
+      height: 2,
+      color: "#fff",
+      value: `${item.title} (${item.released_year})`,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: 0.02, z: 0.05 });
+    return entityEl;
+  },
+  createDescriptionEl: function (item) {
+    const entityEl = document.createElement("a-entity");
+    entityEl.setAttribute("visible", true);
+    entityEl.setAttribute("text", {
+      shader: "msdf",
+      anchor: "left",
+      font: "https://cdn.aframe.io/examples/ui/Viga-Regular.json",
+      width: 0.75,
+      height: 2,
+      color: "#fff",
+      wrapCount: "40",
+      value: item.description,
+    });
+    entityEl.setAttribute("position", { x: -0.4, y: -0.24, z: 0.05 });
+    return entityEl;
+  },
+});
